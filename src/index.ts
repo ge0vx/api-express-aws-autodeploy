c7ad62744c40a427483e639ced5de3af90610413
a3033871@bestbuy.com <a3033871@bestbuy.com>
2023-12-11 10:24:18 -0600

    force port 5000

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import TrackRoutes from "./track/track.routes"
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use('/api', TrackRoutes)
