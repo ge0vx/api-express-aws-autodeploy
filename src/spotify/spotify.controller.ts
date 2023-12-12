26d8a924f9694ae7a4e0728556cabaf504ee003f
a3033871@bestbuy.com <a3033871@bestbuy.com>
2023-12-12 11:57:02 -0600

    create spotify api service

--- src/spotify/spotify.controller.ts
+++ src/spotify/spotify.controller.ts
@@ -1,55 +1,26 @@
 import { Request, Response, NextFunction } from "express";
-import { SpotifyApi } from "@spotify/web-api-ts-sdk";
+import SpotifyService from "./spotify.service";
 import  dotenv from 'dotenv';
 dotenv.config();
 
 export class SpotifyController {
 
-    spotifyApy: SpotifyApi;
+    spotifyService: SpotifyService;
 
     constructor() {
-        const spotifyClientId = process.env.SPOTIFY_CLIENT_ID || "";
-        const spotifySecretKey = process.env.SPOTIFY_SECRET_KEY || "";
-        this.spotifyApy = SpotifyApi.withClientCredentials(
-            spotifyClientId,
-            spotifySecretKey
-        )
+        this.spotifyService = new SpotifyService();
     }
-    async searchByTitle(
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ): Promise<void> {
+    
+    async searchByTitle(req: Request, res: Response, next: NextFunction): Promise<void> {
         const { trackTitle } = req.params;
-        const items = await this.spotifyApy.search(trackTitle, ["track"]);
-        const response = items.tracks.items.map((item) => ({
-            id: item.id,
-            isrc: item.external_ids.isrc,
-            name: item.name,
-            image: item.uri,
-            artist: item.artists.map((artist)=>artist.name),
-            popularity: item.popularity,
-        }));
-
+        const response = await this.spotifyService.searchByTitle(trackTitle)
         res.status(200).json(response);
     }
 
-    async searchById(
-        req: Request,
-        res: Response,
-        next: NextFunction
+    async searchById(req: Request, res: Response, next: NextFunction
     ): Promise<void> {
         const { trackId } = req.params;
-        const item = await this.spotifyApy.tracks.get(trackId);
-        const response = {
-            id: item.id,
-            isrc: item.external_ids.isrc,
-            name: item.name,
-            image: item.uri,
-            artist: item.artists.map((artist)=>artist.name),
-            popularity: item.popularity,
-        }
-
+        const response = await this.spotifyService.searchById(trackId)
         res.status(200).json(response);
     }
 
---
+++ src/spotify/spotify.service.ts
@@ -0,0 +1,43 @@
+import { AccessToken, SpotifyApi } from '@spotify/web-api-ts-sdk';
+
+export default class SpotifyService {
+
+    spotifyApi: SpotifyApi;
+
+    constructor() {
+        const spotifyClientId = process.env.SPOTIFY_CLIENT_ID || "";
+        const spotifySecretKey = process.env.SPOTIFY_SECRET_KEY || "";
+        this.spotifyApi = SpotifyApi.withClientCredentials(
+            spotifyClientId,
+            spotifySecretKey
+        )
+    }
+
+    public async searchByTitle(trackTitle: string): Promise<any> {
+        const items = await this.spotifyApi.search(trackTitle, ["track"]);
+        const response = items.tracks.items.map((item) => ({
+            id: item.id,
+            isrc: item.external_ids.isrc,
+            name: item.name,
+            image: item.uri,
+            artist: item.artists.map((artist)=>artist.name),
+            popularity: item.popularity,
+        }));
+
+        return response;
+    }
+
+    public async searchById(trackId: string): Promise<any> {
+        const item = await this.spotifyApi.tracks.get(trackId);
+        const response = {
+            id: item.id,
+            isrc: item.external_ids.isrc,
+            name: item.name,
+            image: item.uri,
+            artist: item.artists.map((artist)=>artist.name),
+            popularity: item.popularity,
+        }
+
+        return response;
+    }
+}
