2cf80e66dc349d644c3ec0f2a371cfd10c5d0e15
a3033871@bestbuy.com <a3033871@bestbuy.com>
2023-12-12 04:46:22 -0600

    config swagger

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import express from "express";
 import database from "./config/database";
 import TrackRoutes from "./track/track.routes"
 import  dotenv from 'dotenv';
+import swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "./swagger.json"
 dotenv.config();
 
 const port = process.env.PORT || 5000;
@@ -15,6 +17,7 @@ database.initialize()
     .catch(console.error)
 
 app.use('/api', TrackRoutes)
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));    
 
 
 app.listen(port, ()=>{
